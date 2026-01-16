f831e2baf064a3036e18de30c887419aefd4af59
YosraHouas <houas_yora@hotmail.fr>
2026-01-15 21:50:14 -0500

    Corrections main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
